5c2ed9fba39d8c3ff7ab86c563967930aa351c59
antonio <antoniopenteado.aluno@unipampa.edu.br>
2026-04-02 17:03:30 -0300

    commit inicial

---
+++ src/locadoraDeCarros/Cliente.java
@@ -0,0 +1,22 @@
+package locadoraDeCarros;
+
+public class Cliente {
+	
+	String cpf;
+	String nome;
+	String endereco;
+	int nivelDeConfibialidade;
+	double dinheiro;
+	
+	
+	
+	public static void Cliente(){
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/locadoraDeCarros/Main.java
@@ -0,0 +1,5 @@
+package locadoraDeCarros;
+
+public class Main {
+
+}

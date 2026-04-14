9c51dab796985f577533c554b7293d68d2aa1a24
AntonioAugust <antoniopenteado.aluno@unipampa.eu.br>
2026-04-14 16:41:15 -0300

    classe Venda adicionada

---
+++ src/locadoraDeCarros/Venda.java
@@ -0,0 +1,38 @@
+package locadoraDeCarros;
+
+import java.util.Date;
+
+public class Venda {
+    private int idVenda;
+    private Date data;
+    private Veiculo veiculo;
+    private Cliente cliente;
+    private Double valorFinal;
+    private String formaPagamento;
+
+    public Venda(Veiculo veiculo, Cliente cliente, Double valorFinal, String formaPagamento, Date data, int idVenda){
+        this.idVenda = idVenda;
+        this.data = data;
+        this.veiculo = veiculo;
+        this.cliente = cliente;
+        this.valorFinal = valorFinal;
+        this.formaPagamento = formaPagamento;
+    }
+
+    public int getIdVenda(){
+        return idVenda;
+    }
+
+    @Override  //sobrescreve o metodo toString() chamado na funcao de printar (system.out.print(toString()))
+    public String toString(){
+        return "Id_Venda = "+ idVenda +"\nVeiculo = "+veiculo+"\nCliente = "+cliente+"\nData = "+data+"\nValorFinal = "+valorFinal+"\nFormaPagamento = "+formaPagamento+"\n ";
+
+
+
+
+
+    }
+
+
+
+}
